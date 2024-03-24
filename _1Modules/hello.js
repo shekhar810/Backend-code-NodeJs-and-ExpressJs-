874c2b352365f01254124e8e1144cc09546c5d0c
Shekhar Suman <92952271+shekhar810@users.noreply.github.com>
2024-03-24 16:18:11 +0530

    Add files via upload

---
+++ _1Modules/hello.js
@@ -0,0 +1,12 @@
+const {addFn,subFn}=require("./math")
+
+// let r=require("./math")
+
+// console.log(r);
+// console.log(r.(3,7));
+
+// console.log(r.addFn(4,3));
+// console.log(r.subFn(9,3));
+
+console.log(addFn(4,5));
+console.log(subFn(8,3));
